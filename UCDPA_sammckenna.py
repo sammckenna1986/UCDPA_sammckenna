8f1ecfae0f434cb12838678924c69fc249fbbe92
sammckenna1986 <sammckenna1986@gmail.com>
2021-10-31 18:10:08 +0000

    End of project and assignment writeup.

--- UCDPA_sammckenna.py
+++ UCDPA_sammckenna.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score, accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score
 from sklearn.linear_model import LogisticRegression, Ridge
 from sklearn.ensemble import GradientBoostingClassifier
 from imblearn.over_sampling import RandomOverSampler
@@ -17,11 +17,11 @@ def import_data():
     data = pd.read_csv("BankChurners.csv")
     # creating a dataframe
     df =pd.DataFrame(data, columns= ['CLIENTNUM', 'Attrition_Flag', 'Customer_Age', 'Gender', 'Dependent_count', 'Education_Level' ,'Marital_Status', 'Income_Category', 'Card_Category', 'Months_on_book','Total_Relationship_Count','Months_Inactive_12_mon','Contacts_Count_12_mon','Credit_Limit','Total_Revolving_Bal','Avg_Open_To_Buy','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt', 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1','Avg_Utilization_Ratio'])
-    # converting categorical values for numerical values here: Attrition_Flag 'Existing Customer' with '0' and 'Attrited Customer' with '1'
     return df
 
 def exploring_data():
     df = import_data()
+    # converting categorical values for numerical values here: Attrition_Flag 'Existing Customer' with '0' and 'Attrited Customer' with '1'
     df['Attrition_Flag'].replace({'Existing Customer': int(0), 'Attrited Customer': int(1)},
                                  inplace=True)  # https://www.kite.com/python/answers/how-to-replace-column-values-in-a-pandas-dataframe-in-python
     df['Gender'].replace({'M': int(0), 'F': int(1)}, inplace=True)
@@ -46,9 +46,8 @@ def exploring_data():
     # Calling the multiple features X
     X = df[data_features]
 
-    # Explore the data set a bit more and start to look at graphs for anomalies in the type of customers and how the data is setup and what might be an indicator of them leaving.
-    print("The shape of X is:" + str(X.shape))  # results 10127 rows and 10 columns
-    print(df.isnull().sum())  # no missing values
+    # Explore the data set a bit more and start to look at graphs for anomalies in the 'type of customers', 'how the data is setup', and 'what might be an indicator of the customers leaving'.
+    print("The shape of X is:" + str(X.shape))  # results 10127 rows and 20 columns
     print("There are" + str(df.isnull().sum()) + "null values in the dataset")
     print("Describing X:" + str(X.describe()))
     print("The head of X:" + str(X.head()))
@@ -79,14 +78,11 @@ def exploring_data():
     print('Total_Ct_Chng_Q4_Q1', df['Total_Ct_Chng_Q4_Q1'].unique())
     print('Avg_Utilization_Ratio', df['Avg_Utilization_Ratio'].unique())
 
-
-
-
 def CHART_Months_on_BooksVS_attrition():
     df = import_data()
     sns.catplot(data=df, kind="count", x="Months_on_book", y=None,
                 hue="Attrition_Flag", )  # https://seaborn.pydata.org/introduction.html https://seaborn.pydata.org/generated/seaborn.catplot.html
-    plt.show()  # This was interesting as a lof of the customers seem to be there for around 36 months and it might show that the dataset might be skewed or they had a marketing campaign that attracted a lot of customers 36 months ago.
+    plt.show()  # This was interesting as a lot of the customers seem to be there for around 36 months and it might show that the dataset might be skewed or they had a marketing campaign that attracted a lot of customers 36 months ago.
 
 def CHART_ContactsCountVS_attrition():
     df = import_data()
@@ -138,7 +134,7 @@ def KNN(X, y):
     print('Untuned KNN Confusion Matrix: \n', confusion_matrix(y_test, y_pred))
     print('Untuned KNN Classification Report: \n', classification_report(y_test, y_pred))
 
-def hyperparametertuning_onKNN_withridge(X, y):
+def hyperparametertuning_onKNN_withRidge(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=42)
     # Setup the parameters and distributions to sample from: param_dist
     param_dist = {"n_neighbors": [5,6,7,8], "weights": ["uniform","distance"],"algorithm": ["auto", "ball_tree", "kd_tree","brute"], "leaf_size": [30,15,45]}
@@ -148,8 +144,8 @@ def hyperparametertuning_onKNN_withridge(X, y):
     # Instantiate the RandomizedSearchCV object:
     knn_cv = RandomizedSearchCV(knn, param_dist, cv=5)
 
-    # Fit it to the data
-    knn_cv.fit(X, y)
+    # Fit it to the data -- Using the training data only as per: https://towardsdatascience.com/building-a-k-nearest-neighbors-k-nn-model-with-scikit-learn-51209555453a
+    knn_cv.fit(X_train, y_train)
 
     # Print the tuned parameters and score
     print("Tuned Decision KNN Parameters: \n{}".format(knn_cv.best_params_))
@@ -158,14 +154,14 @@ def hyperparametertuning_onKNN_withridge(X, y):
     # Instantiate a ridge regressor: ridge
     ridge = Ridge(alpha=0.5)
 
-    # Perform 5-fold cross-validation: ridge_cv
+    # Perform 5-fold cross-validation: ridge_cv -- using actual X, y as per: https://scikit-learn.org/stable/modules/cross_validation.html
     ridge_cv = cross_val_score(ridge, X, y, cv=5)
 
     # Print the cross-validated scores
     print('The "KNN-HyperparameterTuned - Ridge Cross Validation" scores: \n', ridge_cv) # https://towardsdatascience.com/the-power-of-ridge-regression-4281852a64d6
+    # https://stats.stackexchange.com/questions/154706/how-to-interpret-ridge-regression-plot
 
-
-def logreg_AUC(X, y):
+def logreg_AUC(X, y): #https://www.analyticsvidhya.com/blog/2020/06/auc-roc-curve-machine-learning/
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=42)
 
     # Create the classifier: logreg
@@ -175,9 +171,10 @@ def logreg_AUC(X, y):
     logreg.fit(X_train, y_train)
 
     # Predict the labels of the test set: y_pred
+    # Compute predicted probabilities: y_pred_prob
     y_pred = logreg.predict(X_test)
     y_pred_prob = logreg.predict_proba(X_test)[:, 1]
-    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob) # https://www.analyticsvidhya.com/blog/2020/06/auc-roc-curve-machine-learning/
     plt.plot([0, 1], [0, 1], 'k--')
     plt.plot(fpr, tpr, label='Logistic Regression')
     plt.xlabel('False Positive Rate')
@@ -192,10 +189,12 @@ def logreg_AUC(X, y):
     print("Logistic Regression AUC: \n{}".format(roc_auc_score(y_test, y_pred_prob)))
 
     # Compute cross-validated AUC scores: cv_auc
-    cv_auc = cross_val_score(logreg, X, y, cv=5, scoring='roc_auc')
+    cv_auc = cross_val_score(logreg, X, y, cv=5, scoring='roc_auc') # https://www.mygreatlearning.com/blog/cross-validation/#:~:text=The%20purpose%20of%20cross%E2%80%93validation,generalize%20to%20an%20independent%20dataset.
 
     # Print list of AUC scores
     print("Logistic Regression - AUC scores computed using 5-fold cross-validation: \n{}".format(cv_auc))
+    print("The mean of the 5 folds of AUC scores: ", cv_auc.mean())
+
 
 def SVMwith_gridsearchHyperparametertuning(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=42)
@@ -228,7 +227,7 @@ def SVMwith_gridsearchHyperparametertuning(X, y):
     print('SVM after Gradient boosting - The CLF score is: \n', clf.score(X_test, y_test))
 
 
-def main():
+def main(): #the main function I nested the model functions in.
     df = import_data()
     df['Attrition_Flag'].replace({'Existing Customer': int(0), 'Attrited Customer': int(1)}, inplace=True)
     df['Card_Category'].replace({'Blue': int(1), 'Silver': int(2), 'Gold': int(3), 'Platinum': int(4)})
@@ -244,11 +243,11 @@ def main():
     Scaled_Features = scaler.fit_transform(Features)
     X = Scaled_Features
     y = Customer_Attrition
-    oversample_minority(X,y)
-    #KNN(X,y)
-    #logreg_AUC(X,y)
-    #hyperparametertuning_onKNN_withridge(X,y)
-    SVMwith_gridsearchHyperparametertuning(X,y)
+    oversample_minority(X,y) # oversampling the minority to try and get better model results.
+    KNN(X,y) # Model type 1: K-Nearest Neighbors.
+    hyperparametertuning_onKNN_withRidge(X,y) # Model type 1K-Nearest Neighbors, but now with hyperparameter tuning and ridge regression.
+    logreg_AUC(X, y) # Model type 2: Logistic Regression with 'Area under the ROC curve analysis'.
+    SVMwith_gridsearchHyperparametertuning(X,y) # Model type 3: Support-Vector Machines with parameter tuning and gradient boosting.
 
 def oversample_minority(X, y):
     oversample = RandomOverSampler(sampling_strategy=0.5)
@@ -257,12 +256,11 @@ def oversample_minority(X, y):
     print('After oversampling minority - sampling counts [0=Existing_Customer and 1=Attrited_Customer]: \n', Counter(y))
     return X, y
 
-#main()
+main() # the main function I nested the model functions in.
+#Leaving the below function calls to make it easier to test the separate functions:
 #correlation_analysis()
-CHART_genderVS_platinum_card_program()
-CHART_Months_on_BooksVS_attrition()
-CHART_ContactsCountVS_attrition()
-CHART_TansactionsCountVS_attrition()
-
-
-    #pd.set_option("display.max_rows", None, "display.max_columns", None)
+#CHART_genderVS_platinum_card_program()
+#CHART_Months_on_BooksVS_attrition()
+#CHART_ContactsCountVS_attrition()
+#CHART_TansactionsCountVS_attrition()
+#exploring_data()
